2cdf0c1f5b1222c3cd8b0c201922e01861f5a514
sanjuramu248-web <sanjuramu248@gmail.com>
2025-11-19 16:22:50 +0530

    Delete dist directory

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import cors from "cors";
-import requestRoutes from "./routes/requestRoutes";
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use('/api', requestRoutes);
-export { app };
--- dist/routes/requestRoutes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { makeRequest, getHistory } from '../controller/requestController';
-const router = Router();
-router.post('/request', makeRequest);
-router.get('/history', getHistory);
-export default router;
